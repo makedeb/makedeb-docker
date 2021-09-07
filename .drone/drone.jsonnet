168149fefad16668e2458861e580b11c8099434b
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-09-06 23:29:53 -0500

    Updated tooling to run multiple builds at the same time

--- .drone/drone.jsonnet
+++ .drone/drone.jsonnet
@@ -1,16 +1,19 @@
-local buildImage(type) = {
-  name: "build-image-" + type,
+local buildImage(source_image, target_tag, os_type) = {
+  name: "build-image",
   kind: "pipeline",
   type: "docker",
-  trigger: {event: type},
   volumes: [{name: "docker", host: {path: "/var/run/docker.sock"}}],
+  depends_on: ["clone"],
   steps: [{
-    name: "build-and-publish-" + type,
+    name: "build-and-publish",
     image: "docker",
     volumes: [{name: "docker", path: "/var/run/docker.sock"}],
     environment: {
       proget_api_key: {from_secret: "proget_api_key"},
-      TAG: "stable"
+      TAG: "stable",
+      source_image: source_image,
+      target_tag: target_tag,
+      os_type: os_type
     },
     commands: [
       "apk add --no-cache bash",
@@ -20,7 +23,13 @@ local buildImage(type) = {
 };
 
 [
-  buildImage("cron"),
-  buildImage("custom"),
-  buildImage("push")
+  buildImage("debian:latest", "debian-latest", "debian"),
+  buildImage("debian:buster", "debian-buster", "debian"),
+  buildImage("debian:bullseye", "debian-bullseye", "debian"),
+  buildImage("ubuntu:latest", "ubuntu-latest", "debian"),
+  buildImage("ubuntu:rolling", "ubuntu-rolling", "debian"),
+  buildImage("ubuntu:bionic", "ubuntu-bionic", "debian"),
+  buildImage("ubuntu:focal", "ubuntu-focal", "debian"),
+
+  buildImage("archlinux", "archlinux-latest", "archlinux")
 ]
--- .drone/scripts/main.sh
+++ .drone/scripts/main.sh
@@ -12,97 +12,31 @@ done
 # Log in to ProGet server.
 configure_docker
 
-# Define images to build.
-# These take the syntax of 'image/target_tag' where 'image' is an image name
-# (with optional tag), and 'target_tag' is a comma-separated list of tag suffixes
-#  to publish under (they all get prefixed with the release name such as 'beta_').
-debian_targets=("debian:latest/debian-latest"
-                "debian:buster/debian-buster"
-                "debian:bullseye/debian-bullseye"
-                "ubuntu:latest/ubuntu-latest"
-                "ubuntu:rolling/ubuntu-rolling"
-                "ubuntu:bionic/ubuntu-bionic"
-                "ubuntu:focal/ubuntu-focal")
-
-arch_targets=("archlinux/archlinux-latest")
 
 # Ensure tag was specified.
 case "${TAG}" in
-  stable)     makedeb_package="makedeb"; makepkg_package="makedeb-makepkg" ;;
-  beta|alpha) makedeb_package="makedeb-${TAG}"; makepkg_package="makedeb-makepkg-${TAG}" ;;
+  stable)     makedeb_package="makedeb"; makepkg_package="makedeb-makepkg"; target_tag="${proget_server}/docker/makedeb/makedeb:${target_tag}";;
+  beta|alpha) makedeb_package="makedeb-${TAG}"; makepkg_package="makedeb-makepkg-${TAG}"; target_tag="${proget_server}/docker/makedeb/makedeb:${target_tag}-${TAG}" ;;
   "")         echo "[Error] No tag was specified."; exit 1 ;;
   *)          echo "[Error] Invalid tag '${TAG}'."; exit 1 ;;
 esac
 
-# Build images.
-echo "[Info] Building images..."
+# Setup image.
+echo "[Info] Building 'makedeb/makedeb:${target_tag}' from '${source_image}'..."
 
-built_image_tags=()
+if [[ "${os_type}" == "debian" ]]; then
+  target_dockerfile="Dockerfile.debian"
+elif [[ "${os_type}" == "archlinux" ]]; then
+  target_dockerfile="Dockerfile.archlinux"
+else
+  echo "Invalid os type was passed."
+fi
 
-# Build Debian images.
-for i in "${debian_targets[@]}"; do
+sed "1s|{{image}}|${source_image}|" "./${target_dockerfile}"
 
-  # We get 'source_image' and 'docker_build_arguments' from this function.
-  get_buildargs "${i}"
+# Build and publish image.
+docker build --no-cache \
+             -t "${target_tag}" \
+             "./${target_dockerfile}"
 
-  # Add tags to separate array so we know what tags to upload later.
-  for j in "${docker_build_arguments[@]}"; do
-    if [[ "${j}" != "-t" ]]; then
-      built_image_tags+=("${j}")
-    fi
-  done
-
-    rm -rf Dockerfile.debian.tmp
-    cp Dockerfile.debian Dockerfile.debian.tmp
-
-    sed -i "s|{{image}}|${source_image}|" Dockerfile.debian.tmp
-
-    # Formatted tags.
-    formatted_target_tags="$(echo "${docker_build_arguments[@]}" | sed "s|-t ||g")"
-
-  # Build images.
-  echo "[Info] Building image for '${source_image}' with tags ${formatted_target_tags}."
-  run_verbosely docker build ./ \
-                       -f ./Dockerfile.debian.tmp \
-                       "${docker_build_arguments[@]}" \
-                       --build-arg "proget_url=${proget_server}" \
-                       --build-arg "makedeb_package=${makedeb_package}"
-done
-
-# Build Arch Linux images.
-for i in "${arch_targets[@]}"; do
-  get_buildargs "${i}"
-
-  # Add tags to separate array so we know what tags to upload later.
-  for j in "${docker_build_arguments[@]}"; do
-    if [[ "${j}" != "-t" ]]; then
-      built_image_tags+=("${j}")
-    fi
-  done
-
-  rm -rf Dockerfile.archlinux.tmp
-  cp Dockerfile.archlinux Dockerfile.archlinux.tmp
-
-  sed -i "s|{{image}}|${source_image}|" Dockerfile.archlinux.tmp
-
-  # Formatted tags.
-  formatted_target_tags="$(echo "${docker_build_arguments[@]}" | sed "s|-t ||g")"
-
-  # Build images.
-  echo "[Info] Building image for '${source_image}' with tags ${formatted_target_tags}."
-  run_verbosely docker build ./ \
-                       -f ./Dockerfile.archlinux.tmp \
-                       "${docker_build_arguments[@]}" \
-                       --build-arg "aur_url=${aur_url}" \
-                       --build-arg "makedeb_package=${makedeb_package}" \
-                       --build-arg "makepkg_package=${makepkg_package}"
-done
-
-# Remove temporary Dockerfiles.
-rm Dockerfile.debian.tmp \
-   Dockerfile.archlinux.tmp
-
-# Upload all the of the built images.
-for i in "${built_image_tags[@]}"; do
-  run_verbosely docker push -- "${i}"
-done
+docker push "${target_tag}"
