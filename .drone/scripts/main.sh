9e2a0e5b302007939c91bceeb38b8ee1f0903420
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-09-19 19:55:00 -0500

    Updated tooling to build for all three releases when no tag is specified

--- .drone/scripts/main.sh
+++ .drone/scripts/main.sh
@@ -15,35 +15,40 @@ configure_docker
 
 # Ensure tag was specified.
 case "${TAG}" in
-  stable)     makedeb_package="makedeb"; makepkg_package="makedeb-makepkg" ;;
-  beta|alpha) makedeb_package="makedeb-${TAG}"; makepkg_package="makedeb-makepkg-${TAG}" ;;
-  "")         echo "[Error] No tag was specified."; exit 1 ;;
+  stable)     package=("makedeb/makedeb-makepkg");;
+  beta|alpha) package=("makedeb-${TAG}/makedeb-makepkg-${TAG}");;
+  "")         package=("makedeb/makedeb-makepkg" "makedeb-beta/makedeb-makepkg-beta" "makedeb-alpha/makedeb-makepkg-alpha");;
   *)          echo "[Error] Invalid tag '${TAG}'."; exit 1 ;;
 esac
 
 # Setup image.
-echo "[Info] Building 'makedeb/${makedeb_package}:${target_tag}' from '${source_image}'..."
+for i in "${package[@]}"; do
+  makedeb_package="$(echo "${i}" | awk -F '/' '{print $1}')"
+  makepkg_package="$(echo "${i}" | awk -F '/' '{print $2}')"
+  
+  echo "[Info] Building 'makedeb/${makedeb_package}:${target_tag}' from '${source_image}'..."
 
-if [[ "${os_type}" == "debian" ]]; then
-  target_dockerfile="Dockerfile.debian"
-elif [[ "${os_type}" == "archlinux" ]]; then
-  target_dockerfile="Dockerfile.archlinux"
-else
-  echo "Invalid os type was passed."
-fi
+  if [[ "${os_type}" == "debian" ]]; then
+    target_dockerfile="Dockerfile.debian"
+  elif [[ "${os_type}" == "archlinux" ]]; then
+    target_dockerfile="Dockerfile.archlinux"
+  else
+    echo "Invalid os type was passed."
+  fi
 
-sed -i "1s|{{image}}|${source_image}|" "./${target_dockerfile}"
+  sed -i "1s|{{image}}|${source_image}|" "./${target_dockerfile}"
 
-published_image_path="${proget_server}/docker/makedeb/${makedeb_package}:${target_tag}"
+  published_image_path="${proget_server}/docker/makedeb/${makedeb_package}:${target_tag}"
 
-# Build and publish image.
-docker build --no-cache \
-             -t "${published_image_path}" \
-             -f "./${target_dockerfile}" \
-             --build-arg "proget_url=${proget_server}" \
-             --build-arg "aur_url=${aur_url}" \
-             --build-arg "makedeb_package=${makedeb_package}" \
-             --build-arg "makepkg_package=${makepkg_package}" \
-             ./
+  # Build and publish image.
+  docker build --no-cache \
+               -t "${published_image_path}" \
+               -f "./${target_dockerfile}" \
+               --build-arg "proget_url=${proget_server}" \
+               --build-arg "aur_url=${aur_url}" \
+               --build-arg "makedeb_package=${makedeb_package}" \
+               --build-arg "makepkg_package=${makepkg_package}" \
+               ./
 
-docker push "${published_image_path}"
+  docker push "${published_image_path}"
+done
