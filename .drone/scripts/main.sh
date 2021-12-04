1ae0bec1c51839c0dfb8765a3e1486855218940a
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-12-03 18:28:54 -0600

    Updated build script and Arch Linux Dockerfile to align with monorepo for makedeb and makepkg

--- .drone/scripts/main.sh
+++ .drone/scripts/main.sh
@@ -15,18 +15,15 @@ configure_docker
 
 # Ensure tag was specified.
 case "${TAG}" in
-  stable)     package=("makedeb/makedeb-makepkg");;
-  beta|alpha) package=("makedeb-${TAG}/makedeb-makepkg-${TAG}");;
-  "")         package=("makedeb/makedeb-makepkg" "makedeb-beta/makedeb-makepkg-beta" "makedeb-alpha/makedeb-makepkg-alpha");;
+  stable)     package=("makedeb");;
+  beta|alpha) package=("makedeb-${TAG}");;
+  "")         package=("makedeb" "makedeb-beta" "makedeb-alpha");;
   *)          echo "[Error] Invalid tag '${TAG}'."; exit 1 ;;
 esac
 
 # Setup image.
 for i in "${package[@]}"; do
-  makedeb_package="$(echo "${i}" | awk -F '/' '{print $1}')"
-  makepkg_package="$(echo "${i}" | awk -F '/' '{print $2}')"
-
-  echo "[Info] Building 'makedeb/${makedeb_package}:${target_tag}' from '${source_image}'..."
+  echo "[Info] Building 'makedeb/${i}:${target_tag}' from '${source_image}'..."
 
   if [[ "${os_type}" == "debian" ]]; then
     target_dockerfile="Dockerfile.debian"
@@ -38,7 +35,7 @@ for i in "${package[@]}"; do
 
   sed -i "1s|{{image}}|${source_image}|" "./${target_dockerfile}"
 
-  published_image_path="${proget_server}/docker/makedeb/${makedeb_package}:${target_tag}"
+  published_image_path="${proget_server}/docker/makedeb/${i}:${target_tag}"
 
   # Build and publish image.
   docker build --no-cache \
@@ -47,8 +44,7 @@ for i in "${package[@]}"; do
                -f "./${target_dockerfile}" \
                --build-arg "proget_url=${proget_server}" \
                --build-arg "aur_url=${aur_url}" \
-               --build-arg "makedeb_package=${makedeb_package}" \
-               --build-arg "makepkg_package=${makepkg_package}" \
+               --build-arg "makedeb_package=${i}" \
                ./
 
   docker push "${published_image_path}"
